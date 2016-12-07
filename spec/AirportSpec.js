934d8cd3b277d9fb70f20758990c10df4f5be623
George Eaton <george_eaton@hotmail.co.uk>
2016-12-07 12:24:54 +0000

    airport can confirm planes take off

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,21 +1,23 @@
-  'use strict';
+'use strict';
 
-  describe('Airport', function(){
-    var airport;
-    var plane;
+describe('Airport', function(){
+  var airport;
+  var plane;
 
-    beforeEach(function(){
-      airport = new Airport();
-      plane = jasmine.createSpyObj('plane',['land']);
-    });
+  beforeEach(function(){
+    airport = new Airport();
+    plane = jasmine.createSpyObj('plane',['land', 'takeOff']);
+  });
 
-    it('has no planes on default', function(){
-      expect(airport._planes).toEqual([]);
-    });
+  it('has no planes on default', function(){
+    expect(airport._planes).toEqual([]);
+  });
+
+  describe('landing', function() {
 
     it('can accept a plane for landing', function(){
       airport.landPlane(plane);
-      expect(airport._planes).toEqual([plane]);
+      expect(airport._planes).toContain(plane);
     });
 
     it("should confirm the plane has landed or not", function(){
@@ -23,3 +25,17 @@
       expect(plane.land).toHaveBeenCalled();
     });
   });
+
+  describe('taking off', function() {
+
+    it("should let a plane take off", function() {
+      airport.takeOffPlane(plane);
+      expect(plane.takeOff).toHaveBeenCalled();
+    });
+    it('should remove the plane from the airport', function() {
+      airport.landPlane(plane);
+      airport.takeOffPlane(plane);
+      expect(airport._planes).not.toContain(plane);
+    });
+  });
+});
--- src/Airport.js
+++ src/Airport.js
@@ -2,7 +2,12 @@ var Airport = function(){
   this._planes = [];
 };
 
-Airport.prototype.landPlane = function(plane){
+Airport.prototype.landPlane = function(plane) {
   plane.land();
   this._planes.push(plane);
+};
+
+Airport.prototype.takeOffPlane = function(plane) {
+  plane.takeOff();
+  this._planes.pop(plane);
 };
